d0d7c7fabe83cec05b3b61fa26519dcf9bf4f118
Csachin48 <91484880+Csachin48@users.noreply.github.com>
2023-08-27 19:38:07 +0530

    2 files modified, 3 added

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <main>
-  <header class="brand-name">
-    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-  </header>
-  <section class="content">
-  </section>
-</main>
+<main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+    </section>
+  </main>
   `,
 })
 export class App {
